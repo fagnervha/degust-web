de06837b08a949b957560502960f287bd67db46f
fagnervha <fagner.oliveira.ro@gmail.com>
2025-10-20 21:11:11 -0400

    3.4 fix

--- src/components/store-details/index.js
+++ src/components/store-details/index.js
@@ -17,7 +17,7 @@ import CustomModal from "components/modal";
 import RestaurantReviewModal from "components/store-details/ReviewModal";
 import useScrollToTop from "api-manage/hooks/custom-hooks/useScrollToTop";
 
-const StoreDetails = ({ storeDetails, configData,loading }) => {
+const StoreDetails = ({ storeDetails, configData }) => {
   useScrollToTop();
   const dispatch = useDispatch();
   const [expanded, setExpanded] = useState(true);
@@ -110,7 +110,7 @@ const StoreDetails = ({ storeDetails, configData,loading }) => {
     } else {
       return (
         <CustomContainer>
-          <CustomStackFullWidth spacing={3}>
+          <CustomStackFullWidth spacing={3} mt="1rem">
             {storeDetails?.announcement === 1 && (
               <StoreCustomMessage
                 storeAnnouncement={storeDetails?.announcement_message}
